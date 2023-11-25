3ed70e8cca94ab62948a40894941558edad5ec82
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2023-11-25 11:59:16 -0300

    gerador .csv game objects

--- Assets/Maps/Source/convertmap.py
+++ Assets/Maps/Source/convertmap.py
@@ -1,31 +1,23 @@
 import json
-fileName = "overworld.json"
+level = input()
+fileInputName = f'Map{level}.json'
+fileOutputName = f'../Map{level}_Objects.csv'
 
-with open(fileName, 'r') as file:
+with open(fileInputName, 'r') as file:
 	data = json.load(file)
 	for layer in data["layers"]:
 		if "objects" in layer:
-			#print(layer["name"])
-			outFile = open("Objects.csv", 'w')
-			outFile.write("Type,x,y,width,height,1,2,3,4,5,6\n")
+			outFile = open(fileOutputName, 'w')
 			for object in layer["objects"]:
 				line = object["type"] + ','
 				line += str(object["x"] * 2) + ','
 				line += str(object["y"] * 2) + ','
 				line += str(object["width"] * 2) + ','
 				line += str(object["height"] * 2) + ','
-				if "properties" in object:
-					num_props = len(object["properties"])
-					index = 0
-					for prop in object["properties"]:
-						line += str(prop["value"])
-						if index != (num_props - 1):
-							line += ','
-						index += 1
-					while index < num_props:
-						line += ','
-						index += 1
+				if object["type"] == 'Polygon':
+					points = object["polygon"]
+					for i in range(len(points)):
+						line += str(points[i]['x']) + ' ' + str(points[i]['y']) + ','
 				else:
-					line += ",,,,,"
-				#print(line)
+					line += ",,,"
 				outFile.write(line + '\n')
