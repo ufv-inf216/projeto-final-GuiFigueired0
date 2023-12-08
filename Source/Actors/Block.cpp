406deb59659aa102bd61b544a8268966c6847c54
erick <erick.s.souza@ufv.br>
2023-12-07 22:07:44 -0300

    Efeito Sonoro do Bloco

--- Source/Actors/Block.cpp
+++ Source/Actors/Block.cpp
@@ -7,9 +7,25 @@
 #include "../Components/DrawComponents/DrawSpriteComponent.h"
 #include "../Components/DrawComponents/DrawPolygonComponent.h"
 #include "../Components/ColliderComponents/AABBColliderComponent.h"
+#include "../AudioSystem.h"
 
 Block::Block(Game* game, const std::string &texturePath, int size)
-        :Actor(game)
+        :Actor(game),
+        mAudioTime(0)
 {
     new DrawSpriteComponent(this, "../Assets/Sprites/Blocks/Block"+texturePath+".png", size, size);
+    mSoundHandle = GetGame()->GetSound()->PlaySound("Platform corrupt.mp3");
+    GetGame()->GetSound()->StopSound(mSoundHandle);
+}
+
+void Block::OnUpdate(float deltaTime)
+{
+    if(GetGame()->GetSound()->GetSoundState(mSoundHandle) != SoundState::Playing && abs(mWorldBodyComponent->GetVelocity().x) > 0)
+    {
+        mSoundHandle = GetGame()->GetSound()->PlaySound("Platform corrupt.mp3");
+    }
+    if(GetGame()->GetSound()->GetSoundState(mSoundHandle) == SoundState::Playing && mWorldBodyComponent->GetVelocity().x == 0)
+    {
+        GetGame()->GetSound()->StopSound(mSoundHandle);
+    }
 }
